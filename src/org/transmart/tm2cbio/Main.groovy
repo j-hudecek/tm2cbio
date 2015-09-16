79d44ac05b1a3a426b44434a3bb992893f7d0ec7
Jan Hudecek <j.hudecek@nki.nl>
2015-09-16 12:27:16 +0200

    better output

--- src/org/transmart/tm2cbio/Main.groovy
+++ src/org/transmart/tm2cbio/Main.groovy
@@ -4,7 +4,6 @@ package org.transmart.tm2cbio
  */
 class Main {
     private static void usage() {
-        println("Converter of tranSMART data export files to cBioPortal import files")
         println("usage tm2cbio <mapping file>")
     }
 
@@ -13,11 +12,12 @@ class Main {
             usage();
             return;
         }
+        println("tm2cbio - Converter of tranSMART data export files to cBioPortal import files")
         if (!new File(args[0]).exists()) {
             println("File '" + args[0] + "' doesn't exist");
             return;
         }
-        println("Converting file using mapping file '" + args[0] + "'")
+        println("Converting files using mapping file '" + args[0] + "'")
         Config c = new Config(args[0]);
         Translator.process(c);
         println("Done")
